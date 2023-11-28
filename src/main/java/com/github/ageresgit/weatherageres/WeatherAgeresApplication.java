e872afb0ed2ff9365116b8e6303a774a14751140
ageresgit <ageres61@gmail.com>
2023-11-28 15:01:09 +0200

    Initial commit

---
+++ src/main/java/com/github/ageresgit/weatherageres/WeatherAgeresApplication.java
@@ -0,0 +1,13 @@
+package com.github.ageresgit.weatherageres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAgeresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAgeresApplication.class, args);
+	}
+
+}
